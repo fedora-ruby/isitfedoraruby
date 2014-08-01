44c74f6253d92bbea1e4c9160f5e2badcd560c40
Achilleas Pipinellis <axilleas@axilleas.me>
2014-08-01 13:10:46 +0300

    Add last update date app in footer.

--- config/initializers/app_version.rb
+++ config/initializers/app_version.rb
@@ -1,4 +1,5 @@
 module Git
   REVISION = `SHA1=$(git rev-parse --short HEAD 2> /dev/null); if [ $SHA1 ]; then echo $SHA1; else echo 'unknown'; fi`.chomp
   VERSION = File.exists?(File.join(Rails.root, 'VERSION')) ? File.open(File.join(Rails.root, 'VERSION'), 'r') { |f| GIT_VERSION = f.gets.chomp } : nil
+  DATE = `git show -s --format=%ci HEAD`.chomp
 end
