d1c6c0852dfed2bdb7357f748cb6f062e10698f6
Achilleas Pipinellis <axilleas@axilleas.me>
2014-08-15 16:33:15 +0300

    Add setter for is_review.

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -3,13 +3,20 @@
 # Table name: bugs
 #
 #  id            :integer          not null, primary key
-#  name          :string(255)
-#  bz_id         :string(255)
+#  summary       :string(255)
+#  bugzilla_id   :string(255)
 #  fedora_rpm_id :integer
 #  is_review     :boolean
 #  created_at    :datetime
 #  updated_at    :datetime
 #  last_updated  :string(255)
+#  component     :string(255)
+#  status        :string(255)
+#  needs_sponsor :boolean
+#  assigned_to   :string(255)
+#  opened_by     :string(255)
+#  reported_on   :datetime
+#  modified_on   :datetime
 #
 
 # Find bugs assigned to each packaged gem
@@ -17,6 +24,11 @@ class Bug < ActiveRecord::Base
   belongs_to :fedora_rpm
 
   def url
-    "https://bugzilla.redhat.com/show_bug.cgi?id=#{bz_id}"
+    "https://bugzilla.redhat.com/show_bug.cgi?id=#{bugzilla_id}"
   end
+
+  def review_request?
+    is_review = component == 'Review Request'
+  end
+
 end
