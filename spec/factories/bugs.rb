eac2099252ea2e49907a9ce176cc664e77019593
Achilleas Pipinellis <axilleas@axilleas.me>
2014-08-15 16:31:54 +0300

    Add factory and spec for bugs.

--- spec/factories/bugs.rb
+++ spec/factories/bugs.rb
@@ -3,19 +3,74 @@
 # Table name: bugs
 #
 #  id            :integer          not null, primary key
-#  name          :string(255)
-#  bz_id         :string(255)
+#  summary       :string(255)
+#  bugzilla_id   :string(255)
 #  fedora_rpm_id :integer
 #  is_review     :boolean
 #  created_at    :datetime
 #  updated_at    :datetime
 #  last_updated  :string(255)
+#  component     :string(255)
+#  status        :string(255)
+#  needs_sponsor :boolean
+#  assigned_to   :string(255)
+#  opened_by     :string(255)
+#  reported_on   :datetime
+#  modified_on   :datetime
 #
+
+require 'faker'
+
 FactoryGirl.define do
   factory :bug do
-    name 'Review Request: rubygem-foo - Short summary'
-    bz_id '12345'
-    is_review true
+    summary 'Review Request: rubygem-foo - Short summary'
+    bugzilla_id '12345'
     last_updated '6 days and 3 hours'
+    assigned_to Faker::Internet.email
+    opened_by Faker::Internet.email
+
+    trait :review_true do
+      is_review true
+      component 'Package Review'
+    end
+
+    trait :review_false do
+      is_review false
+    end
+
+    trait :status_new do
+      status 'NEW'
+    end
+
+    trait :status_assigned do
+      status 'ASSIGNED'
+    end
+
+    trait :sponsor_true do
+      needs_sponsor true
+    end
+
+    trait :sponsor_false do
+      needs_sponsor false
+    end
+
+    factory :new_review_needs_sponsor,
+      traits: [:review_true, :status_new, :sponsor_true]
+
+    factory :assigned_review_needs_sponsor,
+      traits: [:review_true, :status_assigned, :sponsor_true]
+
+    factory :new_review,
+      traits: [:review_true, :status_new, :sponsor_false]
+
+    factory :assigned_review,
+      traits: [:review_true, :status_assigned, :sponsor_false]
+
+    factory :new_bug_not_review,
+      traits: [:review_false, :status_new, :sponsor_false]
+
+    factory :assigned_bug_not_review,
+      traits: [:review_false, :status_assigned, :sponsor_false]
+
   end
 end
--- spec/models/bug_spec.rb
+++ spec/models/bug_spec.rb
@@ -3,14 +3,20 @@
 # Table name: bugs
 #
 #  id            :integer          not null, primary key
-#  name          :string(255)
-#  bz_id         :string(255)
+#  summary       :string(255)
+#  bugzilla_id   :string(255)
 #  fedora_rpm_id :integer
 #  is_review     :boolean
 #  created_at    :datetime
 #  updated_at    :datetime
 #  last_updated  :string(255)
-#  is_open       :boolean
+#  component     :string(255)
+#  status        :string(255)
+#  needs_sponsor :boolean
+#  assigned_to   :string(255)
+#  opened_by     :string(255)
+#  reported_on   :datetime
+#  modified_on   :datetime
 #
 
 require 'rails_helper'
@@ -21,21 +27,51 @@ describe Bug do
     expect(create(:bug)).to be_valid
   end
 
-  before(:all) do
+  it 'has valid bugzilla url' do
     @bug = create(:bug)
     @bugzilla_url = 'https://bugzilla.redhat.com/show_bug.cgi?id='
-  end
-
-  it 'has valid bugzilla url' do
     expect(@bug.url).to match(/#{Regexp.quote(@bugzilla_url)}\d+/)
   end
 
-  it 'bug is a Review Request' do
+  it 'has status NEW and is a Review Request' do
+    @bug = create(:new_review)
     expect(@bug.is_review).to eq true
+    expect(@bug.status).to eq 'NEW'
   end
 
-  it 'bug is not a Review Request' do
-    @bug.is_review = false
+  it 'has status ASSIGNED and is a Review Request' do
+    @bug = create(:assigned_review)
+    expect(@bug.is_review).to eq true
+    expect(@bug.status).to eq 'ASSIGNED'
+  end
+
+  it 'has status NEW and is not a Review Request' do
+    @bug = create(:new_bug_not_review)
+    expect(@bug.status).to eq 'NEW'
     expect(@bug.is_review).to_not eq true
   end
+
+  it 'has status ASSIGNED and is not a Review Request' do
+    @bug = create(:assigned_bug_not_review)
+    expect(@bug.status).to eq 'ASSIGNED'
+    expect(@bug.is_review).to_not eq true
+  end
+
+  context 'component Review Request' do
+
+    before(:all) do
+      @bug = create(:bug)
+    end
+
+    it 'is_review set to true' do
+      @bug.component = 'Review Request'
+      expect(@bug.review_request?).to eq true
+    end
+
+    it 'is_review set to false' do
+      @bug.component = 'Bogus Component'
+      expect(@bug.review_request?).to eq false
+    end
+  end
+
 end
